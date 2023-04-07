a22ebbceb4242fe6ee0df0f1842b76ab305f4ac7
HvalaVitalisu <hvalavitalisu@gmail.com>
2023-04-07 22:01:48 +0200

    tasks

---
+++ django/prvi/tasks/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+
+class newTask(forms.Form):
+    task = forms.CharField(label="New Task")
+    #priority = forms.IntegerField(label="Priority", min_value=1,max_value=10)
+
+def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {
+        "tasks": request.session["tasks"]
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = newTask(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            #tasks.append(task)
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html",
+    {
+        "form": newTask()
+    })
